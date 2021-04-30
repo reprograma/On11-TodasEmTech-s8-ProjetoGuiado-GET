5f899435ae1ff2e5bcce97a211d6380bde2e1d6e
Liz alves <elisabete_carente@hotmail.com>
2021-04-30 17:07:57 -0300

    resolucao do exercicio para o lar S8

--- reprogramaflix/src/app.js
+++ reprogramaflix/src/app.js
@@ -1,8 +1,10 @@
-const express = require("express") //chama o express
-const app = express() //executar express
+const express = require("express"); //chama o express
+const app = express(); //executar express
 
-const filmes = require("./routes/filmesRoutes") //chamando todas as rotas
+const filmes = require("./routes/filmesRoutes");
+const series = require("./routes/seriesRoutes"); //chamando todas as rotas
 
-app.use("/filmes", filmes) //colocando a rota raiz
+app.use("/filmes", filmes); //colocando a rota raiz
+app.use("/series", series);
 
-module.exports = app //exportando app
+module.exports = app; //exportando app
