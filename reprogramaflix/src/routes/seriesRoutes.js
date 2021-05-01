e3799d43bf5d23c94def7988ca79fb56ebd8b669
Eny Martins <enyalvesmartins@gmail.com>
2021-05-01 19:45:11 -0300

    Últimas atualizações: resolução do 'Para o Lar' da semana

--- reprogramaflix/src/routes/seriesRoutes.js
+++ reprogramaflix/src/routes/seriesRoutes.js
@@ -3,6 +3,10 @@ const express = require ("express");
 
 const router = express.Router();
 
-router.get("/todos", seriesController.getAll);
+router.get("/all", seriesController.getAll);
+router.get("/title", seriesController.getByTitle);
+router.get("/genre", seriesController.getByGenre);
+router.get("/:id", seriesController.getById);
+
 
 module.exports = router;
