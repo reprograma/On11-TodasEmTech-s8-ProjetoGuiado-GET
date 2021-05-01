2356e830221eaf1201b1f88ef0ce98a018afd9d8
Nayara Walewski <79403619+NayaraWakewski@users.noreply.github.com>
2021-05-01 19:32:40 +0300

    entrega exercicio semana 8

--- reprogramaflix/src/app.js
+++ reprogramaflix/src/app.js
@@ -1,8 +1,10 @@
-const express = require("express") //chama o express
 const app = express() //executar express
 
 const filmes = require("./routes/filmesRoutes") //chamando todas as rotas
+const series = require("./routes/seriesRoutes")// call all routes of series
 
 app.use("/filmes", filmes) //colocando a rota raiz
+app.use("/series", series)//call root
 
 module.exports = app //exportando app
+
