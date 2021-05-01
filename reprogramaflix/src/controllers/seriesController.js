37919aeb182f12de59c1bc647561c1d2821c70ab
Amanda <bagelha@gmail.com>
2021-05-01 14:49:00 -0300

    exercicio paraolar semana 8

---
+++ reprogramaflix/src/controllers/seriesController.js
@@ -0,0 +1,57 @@
+const series = require("../models/series.json")//chama series.json
+
+//return all series : GET/series
+const allSeries = (request, response)=>{
+    response.status(200).json(series)
+}
+
+//return series by ID
+const seriesByID = (request, response)=>{
+    const seriesId = request.params.seriesId
+
+    const id = series.find(serie => serie.id == seriesId)
+
+    if(seriesId == " "|| id == undefined){
+        response.status(404).json([{"alert":"id not found"}])
+    }else{
+        response.status(200).json(id)
+    }
+}
+
+//return series by Title
+const seriesByTitle = (request, response)=>{
+   
+    const seriesTitle = request.query.seriesTitle.toLowerCase()
+
+    const title = series.find(serie => serie.title.toLowerCase().includes(seriesTitle))
+
+    if(seriesTitle == " " || title == undefined){
+        response.status(404).json([{"alert":"title  not found"}])
+    }else{
+        response.status(200).json(title)
+    }
+}
+
+//return series by genre
+const seriesByGenre = (request, response)=>{
+    const genre = request.query.genre
+
+    const serieGenre = series.filter(serie => serie.genre.includes(genre))
+
+    if(genre ==" " || serieGenre == undefined){
+        response.status(404).json({"alert":"genre not found"})
+    }else{
+        response.status(200).json(serieGenre)
+    }
+      
+}
+
+
+
+
+module.exports ={
+    allSeries,
+    seriesByID,
+    seriesByTitle,
+    seriesByGenre
+}
