098d3cc083b0ba613636f662adb7b1ac5ff0a49a
Karla Thamiris <karlatn@outlook.com>
2021-05-01 22:43:25 -0300

    atividade S8 completa

--- reprogramaflix/src/controllers/seriesController.js
+++ reprogramaflix/src/controllers/seriesController.js
@@ -1,6 +1,8 @@
 const series = require("../models/series.json")
 
 const getSeries = (request, response) => {
+
+    //parâmetro de título
     if(request.query.title || request.query.title == ''){     //verifica se o title está sendo passado como parâmetro
         const titulo = request.query.title.toLowerCase()
         const tituloFiltrado = series.find(serie => serie.title.toLowerCase().includes(titulo))
@@ -13,8 +15,33 @@ const getSeries = (request, response) => {
         }else{
             return response.status(200).send(tituloFiltrado)
         }
-    }      
-    return response.status(200).json(series)   
+    }
+    
+    //parâmetro para gênero
+    if(request.query.genre || request.query.genre == ''){ //response não pode ficar dentro do looping porque ele tem que ser mandado só uma vez
+        const genero = request.query.genre
+        let newList = []
+
+        series.forEach(serie => {                       //passar por cada unidade de série dentro da lista series, percorre o json
+
+            for(item of serie.genre){                   //passar pelo array de genre 
+               
+                if(item.includes(genero) && serie.genre.includes(item)){    //verifica se o item contém o gênero enviado na requisição e se serie.genre possui esse item
+                    newList.push(serie)
+                }
+            }
+
+            if(genero == ''){
+                return response.status(400).json([{
+                    'mensagem': 'ERRO: gênero não encontrado'
+                }])
+            }
+        })
+        
+        return response.status(200).send(newList)
+    }
+    //se não for passado nenhum parâmetro, e for a rota "pura", retornará todas as séries, como desejado na rota raiz.
+    return response.status(200).send(series)
 }
 
 const getById = (request, response) => {
@@ -31,16 +58,7 @@ const getById = (request, response) => {
     }
 }
 
-const getByGenre = (request, response) => {
-    const genreInput = series.find(serie => serie.genre)
-    //console.log(genreIput)
-    //const genreFilter = series.find(serie => serie.genre == genreInput)
-
-    response.status(200).json(genreInput)
-}
-
 module.exports = {
     getSeries,
-    getById,
-    getByGenre
+    getById
 }
--- reprogramaflix/src/routes/seriesRoutes.js
+++ reprogramaflix/src/routes/seriesRoutes.js
@@ -3,8 +3,7 @@ const controllerSerie = require("../controllers/seriesController")
 const express = require("express")
 const router = express.Router()
 
-router.get("/genre", controllerSerie.getByGenre)
 router.get("/:idInput", controllerSerie.getById)
-router.get('*', controllerSerie.getSeries)
+router.get('*', controllerSerie.getSeries) //* inclui todas as rotas não especificadas em rotas
 
 module.exports = router
