20b3f997f222a92452f4361936b6d46c3b23e54a
Thayanne Mendes Viana <thay489@gmail.com>
2021-05-03 12:14:48 -0500

    resolução dos exercicios,incompleto

--- paraolar/src/routes/seriesRoutes.js
+++ paraolar/src/routes/seriesRoutes.js
@@ -4,5 +4,9 @@ const express = require ("express")
 const router = express.Router()
 
 router.get("/todos", controller.getAll)
+router.get("/title", controller.getByTitle)
+router.get("/genre", controller.getByGenre)
+router.get("/:id", controller.getById)
+
 
 module.exports = router
